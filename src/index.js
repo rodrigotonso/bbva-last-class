61c079309d43885b8641600c6de07265f965b9a5
Rodrigo Tonso <67134489+rodrigotonso@users.noreply.github.com>
2023-06-30 13:37:10 -0300

    setup react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import router from "./router/router";
-import App from "./components/App/App";
-import { RouterProvider } from "react-router-dom";
+import Router from "./router/Router";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <RouterProvider router={router}/>
+        <Router />
     </React.StrictMode>
 );
