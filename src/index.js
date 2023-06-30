c02602ad07ec4b0448bc805b7f5339e5a7fbcdb2
Rodrigo Tonso <67134489+rodrigotonso@users.noreply.github.com>
2023-06-30 14:23:00 -0300

    redux config set

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import Router from "./router/Router";
+import { store } from "./store/store";
+import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <Router />
+        <Provider store={store}>
+            <Router />
+        </Provider>
     </React.StrictMode>
 );
